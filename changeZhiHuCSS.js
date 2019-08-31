dd1beadeb30b0e3b48182d9f3eb58f6dc8cc8b24
CH <45276259+chthub@users.noreply.github.com>
2019-08-31 19:59:21 +0800

    知乎全盘护眼色，相比于版本1系列，对样式有很大的修改

--- changeZhiHuCSS.js
+++ changeZhiHuCSS.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         知乎排版修改
 // @namespace    https://github.com/chthub/changeZhiHuCSS
-// @version      1.0.4
+// @version      2.0
 // @author       CH
 // @description  修改了一些排版样式
 // @match        https://www.zhihu.com/follow
@@ -28,17 +28,23 @@
                 // 定制主框
                 var node3 = document.querySelector(".Topstory-container")
                 node3.style.width = '97%'
+                node3.style.backgroundColor = '#d8e2c8'
                 var inner = document.querySelector('.Topstory-mainColumn')
                 inner.style.width = '100%'
                 inner.style.fontFamily = 'Times New Roman,楷体'
-                inner.style.fontSize = '23px'
+                inner.style.fontSize = '22px'
                 // 定制评论栏
                 let doCSS = document.createElement('style')
-                doCSS.innerHTML += '.RichContent-actions{height:12px !important}'
-                doCSS.innerHTML += '.VoteButton{color:#999 !important;background: #d8e2c8 !important}'
+                doCSS.innerHTML += '.RichContent-actions{height:12px !important;}'
+                doCSS.innerHTML += ".Topstory-noMarginCard,.TopstoryItem-isRecommend,.ContentItem-actions,.AppHeader,#root,.TopstoryItem,.HotItem,.HotListNav-wrapper,.CommentsV2 ,.Topbar,.CommentsV2-footer{background-color: #d8e2c8 !important}"
+                doCSS.innerHTML += '.VoteButton{color:#999 !important;background: #d8e2c8 !important;height:12px !important}'
+                doCSS.innerHTML += ".ContentItem-title {font-size: 22px;}"
+                doCSS.innerHTML += ".HotListNav-item{background-color: #bad889 !important}"
                 //图片和视频的显示区域大小
                 doCSS.innerHTML += '.origin_image{max-width:500px !important}'
                 doCSS.innerHTML += '.RichText-video{max-width:500px !important}'
+                //文章内代码区
+                doCSS.innerHTML += '.highlight pre{font-size:medium !important;background-color: #cbde8c !important;}'
                 document.getElementsByTagName('head')[0].appendChild(doCSS)
                 // 固定顶部导航栏
                 let header = document.getElementsByTagName('header')[0]
@@ -55,27 +61,27 @@
         (function () {
             //修改专栏样式
             if (path.match(/\/p\/\d+/g)) {
-                //左侧的点赞框
-                window.onscroll = function () {
-                    let inner1 = document.querySelector(".Post-SideActions")
-                    if (inner1) {
-                        inner1.style.visibility = "hidden"
+                    window.onload = function () {
+                        //左侧的点赞框
+                    window.onscroll = function () {
+                        let inner1 = document.querySelector(".Post-SideActions")
+                        if (inner1) {
+                            inner1.style.visibility = "hidden"
+                        }
                     }
-                }
-                //背景颜色
-                var backG = document.querySelector('.Post-content')
-                backG.style.backgroundColor = '#d8e2c8'
-                //正文
-                var contentTxt = document.querySelector('.Post-RichTextContainer')
-                contentTxt.style.width = '95%'
-                contentTxt.style.fontSize = '30px'
-                contentTxt.style.fontFamily = 'Times New Roman,楷体'
-                contentTxt.style.backgroundColor = '#d8e2c8'
+                    //上方的导航栏
+                    var node = document.querySelector('.ColumnPageHeader-Wrapper')
+                    node.remove()
+                    //背景颜色
+                    var backG = document.querySelector('.Post-content')
+                    backG.style.backgroundColor = '#d8e2c8'
+                    //正文
+                    var contentTxt = document.querySelector('.Post-RichTextContainer')
+                    contentTxt.style.width = '95%'
+                    contentTxt.style.fontSize = '30px'
+                    contentTxt.style.fontFamily = 'Times New Roman,楷体'
+                    contentTxt.style.backgroundColor = '#d8e2c8'
 
-                window.onload = function () {
-                    //上方的框
-                    var re = document.querySelector('.ColumnPageHeader-Wrapper')
-                    re.remove()
                     //右侧的反馈框
                     var inner2 = document.querySelector('.CornerButtons')
                     inner2.remove()
@@ -91,11 +97,13 @@
                     inner9.style.backgroundColor = '#d8e2c8'
                     // 评论栏定制
                     let doCSS = document.createElement('style')
-                    doCSS.innerHTML += '.RichContent-actions{bottom:auto !important;background-color: #d8e2c8 !important}'
-                    doCSS.innerHTML += '.VoteButton{background: #d8e2c8 !important}'
+                    doCSS.innerHTML += '.RichContent-actions {bottom:auto !important;background-color: #d8e2c8 !important}'
+                    doCSS.innerHTML += '.VoteButton,.ContentItem-actions,.Topbar,.CommentsV2-footer,.App-main{background-color: #d8e2c8 !important}'
                     //图片和视频的显示区域大小
-                    doCSS.innerHTML += '.origin_image{max-width:500px !important}'
+                    doCSS.innerHTML += '.origin_image,.TitleImage{max-width:500px !important}'
                     doCSS.innerHTML += '.RichText-video{max-width:500px !important}'
+                    //文章内代码区
+                    doCSS.innerHTML += '.highlight pre{font-size:medium !important;background-color: #cbde8c !important;}'
                     document.getElementsByTagName('head')[0].appendChild(doCSS)
                 }
 
@@ -122,10 +130,17 @@
                     let doCSS = document.createElement('style')
                     doCSS.innerHTML += '.RichContent-actions{bottom:auto !important}'
                     doCSS.innerHTML += '.VoteButton{color:#999 !importantbackground: none !important}'
+                    doCSS.innerHTML += '.QuestionHeader,.QuestionHeader-footer,#root,.List,.ContentItem-actions,.CommentsV2,.Topbar,.CommentsV2-footer{background-color: #d8e2c8 !important}'
                     //图片和视频的显示区域大小
                     doCSS.innerHTML += '.origin_image{max-width:500px !important}'
                     doCSS.innerHTML += '.RichText-video{max-width:500px !important}'
+                    //文章内代码区
+                    doCSS.innerHTML += '.highlight pre{font-size:medium !important;background-color: #cbde8c !important;}'
                     document.getElementsByTagName('head')[0].appendChild(doCSS)
+                    // 顶部导航栏移除
+                    let header = document.querySelector('.AppHeader')
+                    header.remove()
+
                 }
             }
         })();
@@ -138,17 +153,23 @@
             //修改正文区域
             var inner0 = document.querySelector('.Search-container')
             inner0.style.width = '90%'
-            inner0.style.fontSize = '26px'
+
             var inner1 = document.querySelector('.SearchMain')
             inner1.style.width = '100%'
             inner1.style.fontFamily = 'Times New Roman,楷体'
-            // 评论栏定制
+            inner1.style.fontSize = '26px !important'
+            // 定制评论栏
             let doCSS = document.createElement('style')
-            doCSS.innerHTML += '.RichContent-actions{height:5% !important}'
-            doCSS.innerHTML += '.VoteButton{color:#999 !importantbackground: none !important}'
+            doCSS.innerHTML += '.RichContent-actions{height:12px !important;}'
+            doCSS.innerHTML += ".Topstory-noMarginCard,.TopstoryItem-isRecommend,.ContentItem-actions,.AppHeader,#root,.TopstoryItem,.SearchTabs,.HotItem,.HotListNav-wrapper,.CommentsV2 ,.Topbar,.CommentsV2-footer,.SearchResult-Card{background-color: #d8e2c8 !important}"
+            doCSS.innerHTML += '.VoteButton{color:#999 !important;background: #d8e2c8 !important;height:12px !important}'
+            doCSS.innerHTML += ".ContentItem-title {font-size: 22px !important;};.RichText {font-size: 20px !important;}"
+            doCSS.innerHTML += ".HotListNav-item{background-color: #bad889 !important}"
             //图片和视频的显示区域大小
             doCSS.innerHTML += '.origin_image{max-width:500px !important}'
             doCSS.innerHTML += '.RichText-video{max-width:500px !important}'
+            //文章内代码区
+            doCSS.innerHTML += '.highlight pre{font-size:medium !important;background-color: #cbde8c !important;}'
             document.getElementsByTagName('head')[0].appendChild(doCSS)
         }
     })();
